e91e14cb57bb5fb30e47b977d7385465c4585057
Mark McTamney <30511006+markmctamney@users.noreply.github.com>
2021-07-27 08:44:28 -0400

    initial commit

---
+++ install-packages.sh
@@ -0,0 +1,30 @@
+# Dev + Peer
+yarn add -D -P \
+    eslint \
+    prettier \
+    ;
+
+# Dev
+yarn add -D typescript \
+    ;
+
+# Deps
+yarn add \
+    eslint-config-universe \
+    eslint-import-resolver-typescript \
+    eslint-plugin-graphql \
+    eslint-plugin-import \
+    eslint-plugin-json \
+    eslint-plugin-json-format \
+    eslint-plugin-json-schema-validator \
+    eslint-plugin-jsonc \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-monorepo \
+    eslint-plugin-react \
+    eslint-plugin-react-hooks \
+    eslint-plugin-react-native \
+    eslint-plugin-yaml \
+    @typescript-eslint/eslint-plugin \
+    @typescript-eslint/parser \
+    jsonc-eslint-parser \
+    ;
